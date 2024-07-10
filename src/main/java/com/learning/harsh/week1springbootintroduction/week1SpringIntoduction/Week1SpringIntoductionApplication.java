a2d4dcf8062a8238207a96c7bcaba0ac19e252e0
Harsh <hjha0695@gmail.com>
2024-07-10 21:57:30 +0530

    spring-boot initial setup

---
+++ src/main/java/com/learning/harsh/week1springbootintroduction/week1SpringIntoduction/Week1SpringIntoductionApplication.java
@@ -0,0 +1,13 @@
+package com.learning.harsh.week1springbootintroduction.week1SpringIntoduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week1SpringIntoductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week1SpringIntoductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/harsh/week1springbootintroduction/week1SpringIntoduction/Week1SpringIntoductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.harsh.week1springbootintroduction.week1SpringIntoduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week1SpringIntoductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
